8170fdfa6a67f48d8ef29d6066ed553463daec5a
PAPER SUS <zhaowenrui_8866@163.com>
2025-09-08 20:48:36 +0800

    Fix: ensure that the directory exists

--- scripts/course_info.py
+++ scripts/course_info.py
@@ -3,6 +3,7 @@
 # @Author  : papersus
 # @File    : course_info.py
 import json
+import os
 import tju_autocourse as atc
 
 
@@ -12,5 +13,7 @@ if __name__ == "__main__":
     atc.set_config_meta(config["meta"])
     user = atc.create_user(config["users"][0])
     print(user.config.courses_info)
+    if not os.path.exists("./data"):
+        os.mkdir("./data")
     with open("./data/course_info.json", "w", encoding="utf-8") as f:
         json.dump(user.config.courses_info, f, ensure_ascii=False, indent=4)
--- scripts/course_statu.py
+++ scripts/course_statu.py
@@ -3,6 +3,7 @@
 # @Author  : papersus
 # @File    : course_statu.py
 import json
+import os
 import tju_autocourse as atc
 
 
@@ -12,5 +13,7 @@ if __name__ == "__main__":
     atc.set_config_meta(config["meta"])
     user = atc.create_user(config["users"][0])
     print(user.scheduler.course_status)
+    if not os.path.exists("./data"):
+        os.mkdir("./data")
     with open("./data/course_statu.json", "w", encoding="utf-8") as f:
         json.dump(user.scheduler.course_status, f, ensure_ascii=False, indent=4)
